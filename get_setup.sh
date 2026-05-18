686fdaba4ab524dd9abd43a3e98eec38d78c857c
Jose F Rivas C <josefelixrc7@gmail.com>
2026-05-18 14:04:34 -0400

    feat: Fix sh files

--- get_setup.sh
+++ get_setup.sh
@@ -1,3 +1,1 @@
-# bash
-
 !curl https://raw.githubusercontent.com/josefelixrc7/qwen_image_edit_setup/refs/heads/master/setup.sh | bash
--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo "🚀 Starting ComfyUI + Manager + Qwen Models Setup..."
 
 # -------------------------------
